770e9120e35c4ec0b603153af69ec62715bb8be2
Mr.Blue <silverhugh.77@gmail.com>
2021-06-26 10:57:06 +0800

    update source

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,33 @@
 <template>
-  <div class="bg-gray-800">
-    <SandTable />
+  <div class="">
+    <!-- <SandTable /> -->
+    <p>
+      This website has been moved to <a class="text-blue-800 underline" :href="new_url">{{ new_url }}</a
+      >.
+    </p>
+    <p>Redirect in 5s ...</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import SandTable from '@/components/SandTable.vue'
+// import SandTable from '@/components/SandTable.vue'
 
 export default {
-  name: 'Home',
+  name: "Home",
+  data() {
+    return {
+      new_url: "https://tacticalboard.github.io/",
+    };
+  },
+  mounted: function () {
+    console.log(this.new_url)
+    setTimeout(() => {
+      window.location.href = this.new_url;
+    }, 5000);
+  },
   components: {
-    SandTable
-  }
-}
+    // SandTable
+  },
+};
 </script>
